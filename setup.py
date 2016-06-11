d9f642000c5242b8897c0f71df71786aa5da327b
Michael <michaelmartinez@me.com>
2016-06-10 21:06:25 -0700

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Py PostMates',
+      version='1.0',
+      description='A client library for the PostMates API',
+      author='Michael Martinez',
+      author_email='michaelmartinez@me.com',
+      url='https://github.com/iMerica/py-postmates',
+      packages=['postmates'],
+     )
+
